b557bc16625068fc88642e7654012e7361882ea1
salehkheiri1995-png <salehkheiri1995@gmail.com>
2026-02-08 12:02:48 +0330

    🔥 FIX: Corrected Startup Form to MainForm
    FIXES:
    =======
    - Changed `Application.Run(new Form1())` back to `Application.Run(new MainForm())`
    - Confirmed class name from MainForm.cs file
    - This resolves the CS0246 "type or namespace name 'Form1' could not be found" error.
    
    READY TO USE:
    =============
    Pull and run - application will start correctly now!

--- PersonnelManagementApp/Program.cs
+++ PersonnelManagementApp/Program.cs
@@ -13,7 +13,7 @@ namespace PersonnelManagementApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
